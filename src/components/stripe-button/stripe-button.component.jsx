c04d4552fb7758a5b2535edeb37203d2921a242a
Djordje Vasic <djvasic@domain.local>
2021-01-18 18:33:01 +0100

    redux saga for all functionality

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HwSWLGvSFafqQhlzia9xYAOy0Xr6kRJnUUDE3sPZ8l8wtv9uQbKDcRNkZKHYfBXEPHi8DTzMhLqt9YX21mXiL7w00AayeJj77';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
